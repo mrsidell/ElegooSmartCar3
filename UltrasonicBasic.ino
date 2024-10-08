50c127ed7b284edcf6b494df4a69b8eb3306a2d4
mrsidell <jsidell@nohum.k12.ca.us>
2024-10-08 13:09:22 -0700

    Update UltrasonicBasic.ino
    Moved function definitions to bottom.

--- UltrasonicBasic.ino
+++ UltrasonicBasic.ino
@@ -15,6 +15,63 @@ int Trig = A5;
 #define carSpeed 250
 int rightDistance = 0, leftDistance = 0, middleDistance = 0;
 
+void setup() { 
+  myservo.attach(3,700,2400);  // attach servo on pin 3 to servo object
+  Serial.begin(9600);     
+  pinMode(Echo, INPUT);    
+  pinMode(Trig, OUTPUT);  
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  stop();
+} 
+
+void loop() { 
+    myservo.write(90);  //setservo position according to scaled value
+    delay(500); 
+    middleDistance = Distance_test();
+
+    if(middleDistance <= 40) {     
+      stop();
+      delay(500);                         
+      myservo.write(10);          
+      delay(1000);      
+      rightDistance = Distance_test();
+      
+      delay(500);
+      myservo.write(90);              
+      delay(1000);                                                  
+      myservo.write(180);              
+      delay(1000); 
+      leftDistance = Distance_test();
+      
+      delay(500);
+      myservo.write(90);              
+      delay(1000);
+      if(rightDistance > leftDistance) {
+        right();
+        delay(360);
+      }
+      else if(rightDistance < leftDistance) {
+        left();
+        delay(360);
+      }
+      else if((rightDistance <= 40) || (leftDistance <= 40)) {
+        back();
+        delay(180);
+      }
+      else {
+        forward();
+      }
+    }  
+    else {
+        forward();
+    }                     
+}
+// FUNCTION DEFINITIONS
 void forward(){ 
   analogWrite(ENA, carSpeed);
   analogWrite(ENB, carSpeed);
@@ -73,60 +130,5 @@ int Distance_test() {
   return (int)Fdistance;
 }  
 
-void setup() { 
-  myservo.attach(3,700,2400);  // attach servo on pin 3 to servo object
-  Serial.begin(9600);     
-  pinMode(Echo, INPUT);    
-  pinMode(Trig, OUTPUT);  
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
-  pinMode(ENA, OUTPUT);
-  pinMode(ENB, OUTPUT);
-  stop();
-} 
 
-void loop() { 
-    myservo.write(90);  //setservo position according to scaled value
-    delay(500); 
-    middleDistance = Distance_test();
-
-    if(middleDistance <= 40) {     
-      stop();
-      delay(500);                         
-      myservo.write(10);          
-      delay(1000);      
-      rightDistance = Distance_test();
-      
-      delay(500);
-      myservo.write(90);              
-      delay(1000);                                                  
-      myservo.write(180);              
-      delay(1000); 
-      leftDistance = Distance_test();
-      
-      delay(500);
-      myservo.write(90);              
-      delay(1000);
-      if(rightDistance > leftDistance) {
-        right();
-        delay(360);
-      }
-      else if(rightDistance < leftDistance) {
-        left();
-        delay(360);
-      }
-      else if((rightDistance <= 40) || (leftDistance <= 40)) {
-        back();
-        delay(180);
-      }
-      else {
-        forward();
-      }
-    }  
-    else {
-        forward();
-    }                     
-}
 
